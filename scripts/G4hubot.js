2c46427d0a43d120d48b073183254d90997a1c59
Vu Ngoc Quang <quang.vn@outlook.com>
2019-03-07 14:00:31 +0700

    update some text

--- scripts/G4hubot.js
+++ scripts/G4hubot.js
@@ -1,12 +1,7 @@
+// Commands:
+//   hubot flip - flip something
+
 module.exports = (robot) => {
-  robot.respond(/help/i, (res) => {
-      res.send('I will now respond to this message.');
-  });
-
-  robot.hear(/support/i, (res) => {
-    res.send('Message hear.')
-  });
-
   robot.hear(/\[To:2506790\]/i, (res) => {
     console.log(res)
     res.send(`[rp aid=${res.message.user.id} to=${res.message.room}-${res.message.id}] What do you want?`)
