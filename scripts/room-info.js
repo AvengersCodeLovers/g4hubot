81107411c2539dc78a54b8cb23b8bab6546b43c2
Vu Ngoc Quang <quang.vn@outlook.com>
2019-03-08 10:54:56 +0700

    update description

--- scripts/room-info.js
+++ scripts/room-info.js
@@ -3,38 +3,27 @@
 
 yaml = require('js-yaml')
 Chatwork = require('hubot-chatwork')
-
-Object.byString = function(o, s) {
-    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-    s = s.replace(/^\./, '');           // strip a leading dot
-    var a = s.split('.');
-    for (var i = 0, n = a.length; i < n; ++i) {
-        var k = a[i];
-        if (k in o) {
-            o = o[k];
-        } else {
-            return;
-        }
-    }
-    return o;
-}
+get = require('lodash.get')
 
 module.exports = (robot) => {
 
   let cw = robot.adapter.bot
-  console.log(cw.rooms)
 
   for (let i = 0; i < cw.rooms.length; i++) {
-    // robot.brain.set 'description', yaml.safeLoad()
     cw.Room(cw.rooms[i]).show((err, data) => {
       robot.brain.set(`${cw.rooms[i]}-info`, yaml.safeLoad(data.description))
     })
   }
 
+  robot.hear("[info][title][dtext:chatroom_chat_edited]", (res) => {
+    let room = res.message.room
+    cw.Room(room).show((err, data) => {
+      robot.brain.set(`${res.id}-info`, yaml.safeLoad(data.description))
+    })
+  })
+
   robot.respond(/tell me (.*)/i, (res) => {
     let query = res.match[1]
-    console.log(res.message.room)
-    console.log(robot.brain.get(`${res.message.room}-info`))
-    res.reply(Object.byString(robot.brain.get(`${res.message.room}-info`), query))
+    res.reply(get(robot.brain.get(`${res.message.room}-info`), query))
   })
 }
