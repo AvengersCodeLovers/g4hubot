21a41001e7a9da06c53807b07315ef69d9284a9a
Vu Ngoc Quang <quang.vn@outlook.com>
2019-04-19 16:33:40 +0700

    using own chatwork adapter

--- scripts/G4hubot.js
+++ scripts/G4hubot.js
@@ -2,11 +2,11 @@
 //   hubot flip - flip something
 
 module.exports = (robot) => {
-  robot.messageRoom(process.env.HUBOT_CHATWORK_ROOMS, "I'm here 8-)")
+  robot.messageRoom(process.env.HUBOT_CHATWORK_ROOMS, `I'm here 8-).\nMy name is ${process.env.HUBOT_NAME}`)
 
   robot.hear(/\[To:2506790\]/i, (res) => {
     console.log(res)
-    res.send(`[rp aid=${res.message.user.id} to=${res.message.room}-${res.message.id}] What do you want?`)
+    res.reply("What do you want?")
   })
 
   robot.respond(/call (.*) for me/i, (res) => {
@@ -15,7 +15,7 @@ module.exports = (robot) => {
   })
 
   robot.respond('/upgrade/i', (res) => {
-    res.send("(roger) I'll deploy myself")
+    res.reply("(roger) I'll deploy myself")
     setTimeout(() => {
       const shell = require('shelljs');
       if (shell.exec('./upgrade.sh production').code !== 0) {
